21ddddebdbcc83e6a025494e50213230e9e0a6e3
Jamie Reardon <jamie.reardon91@gmail.com>
2022-07-25 22:33:38 +0100

    add categories

---
+++ src/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+  { name: 'Star Wars', path: '/star-wars' },
+  { name: 'Marvel', path: '/marvel' },
+  { name: 'DC Comics', path: '/dc-comics' }
+];
+
+
+export default categories;
