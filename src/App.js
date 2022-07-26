d5733ca45645fcc2eceb50cf9c88fc12d4ef5436
Jamie Reardon <jamie.reardon91@gmail.com>
2022-07-26 13:09:38 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route, Outlet, useLocation } from 'react-router-dom';
 
 import API_KEY from './config';
 import categories from './categories';
 import * as Components from './components';
 import './App.css';
 
+const { Layout, Loader, ErrorMessage, Results } = Components;
+
 const App = () => {
-  const key = API_KEY;
   const [currentSearch, setSearch] = useState(null);
   const [photos, setPhotos] = useState({ app: {}, search: {} });
   const [isLoaded, setLoaded] = useState(false);
-  const { Layout, Loader, ErrorMessage, Results } = Components;
+  const location = useLocation();
 
   const handleSearch = (value, isUser = true) => {
     setLoaded(false);
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${key}&tags=${value}&per_page=24&format=json&nojsoncallback=1`)
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=${value}&per_page=24&format=json&nojsoncallback=1`)
       .then((res) => {
         setPhotos({ ...photos, ...(isUser ? { search: res.data.photos.photo } : { app: { ...photos.app, [value]: res.data.photos.photo } }) });
         setSearch(value);
@@ -25,26 +26,29 @@ const App = () => {
       .catch((err) => console.error(`Error getting photos with the API: ${err}`));
   }
 
+  useEffect(() => {
+    const search = location.pathname.slice('8');
+    location.pathname === `/search/${search}` && handleSearch(search);
+  }, [location.pathname])
+
   return (
-    <BrowserRouter>
-      <div className="container">
+    <div className="container">
 
-        <Layout categories={categories} photos={photos} handleSearch={handleSearch} />
-  
-        <Routes>
-          <Route path="/" element={<Outlet />} />
-          {categories.map(category => (
-            <Route
-              key={category.name}
-              path={`/${category.path}`}
-              element={isLoaded ? <Results search={category.name} photos={photos.app[category.name]} /> : <Loader text="Loading, please wait..." />} />
-          ))}
-          <Route path="/search/:query" element={isLoaded ? <Results search={currentSearch} photos={photos.search} /> : <Loader text="Loading, please wait..." />} />
-          <Route path="*" element={<ErrorMessage title="Page not found" text="The requested page does not exist." />} />
-        </Routes>
+      <Layout categories={categories} photos={photos} handleSearch={handleSearch} />
 
-      </div>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Outlet />} />
+        {categories.map(category => (
+          <Route
+            key={category.name}
+            path={`/${category.path}`}
+            element={isLoaded ? <Results search={category.name} photos={photos.app[category.name]} /> : <Loader text="Loading, please wait..." />} />
+        ))}
+        <Route path="/search/:query" element={isLoaded ? <Results search={currentSearch} photos={photos.search} /> : <Loader text="Loading, please wait..." />} />
+        <Route path="*" element={<ErrorMessage title="Page not found" text="The requested page does not exist." />} />
+      </Routes>
+
+    </div>
   );
 }
 
